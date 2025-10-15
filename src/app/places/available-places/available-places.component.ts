2a7a31542e3268cef0bab38435248d8d39030403
Karan02 <krnptl1234@gmail.com>
2025-10-15 23:35:34 +0530

    learning http requests and responses

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-
+import { catchError, map , throwError} from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
@@ -15,14 +15,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef)
   // constructor(private httpClient: HttpClient){}
 
   ngOnInit(){
-    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log(resData.places);
+    this.isFetching.set(true)
+    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places',{
+      // observe: 'response' // 'event'
+    })
+    .pipe(
+      map(
+        (resData) => resData.places,
+        catchError((error,obs) => throwError(() => new Error("Something went wrong fething the available places. Please try later")))
+      )
+    )
+    .subscribe({
+      next: (places) => { // ' event' or 'response'  
+        // console.log(response);
+        // console.log(response.body?.places);
+        // console.log(event)
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+      error: (error: Error) => {
+        console.log(error);
+        this.error.set(error.message);
       }
     });
 
